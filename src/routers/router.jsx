4836184a73f9b849753b05346451741bb3556f2f
theerpalashivakumar <shivakumartheerpal@gmail.com>
2024-12-27 22:00:16 +0530

    setting tailwind css

---
+++ src/routers/router.jsx
@@ -0,0 +1,22 @@
+import App from "../App";
+import {createBrowserRouter} from "react-router-dom"
+
+
+const router =createBrowserRouter([
+    {
+        path:'/',
+        element:<App/> ,
+        // children:[
+        //     {
+        //         path:'/',
+        //         element:<Home/>
+        //     },
+        //     {
+
+        //     }
+        // ]
+    }
+])
+
+export default router
+
